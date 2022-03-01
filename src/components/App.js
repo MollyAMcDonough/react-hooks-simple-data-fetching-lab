a08546fdfffaec7651c43f7233557bfbfac5e445
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-01 10:47:05 -0500

    commit tree 573934a3eb328c5c28f138fa07a909860a5fe55d

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [loading, setLoadingState] = useState(true);
+    const [src, setSrc] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp) => resp.json())
+        .then((data) => {
+            setSrc(data.message);
+            setLoadingState(false);
+        })
+    },[])
+
+    if (loading) return  <p>Loading...</p>;
+
+    return <img src={src} alt="A Random Dog" />;
+}
+
+export default App;
